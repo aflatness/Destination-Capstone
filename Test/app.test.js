6e579e6dd9700c8155a2d0c4b41e49a3219eaaa2
aflatness <aflatness@gmail.com>
2021-01-15 16:02:49 -0700

    import axios in app tests

--- Test/app.test.js
+++ Test/app.test.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import axios from 'axios';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../Client/components/app.jsx';
--- Test/server.test.js
+++ Test/server.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 const supertest = require('supertest');
 const app = require('../server.js');
-const { Hosts } = require('../Database');
+const { Hosts, db } = require('../Database');
 
 const request = supertest(app);
 
@@ -14,6 +14,8 @@ beforeAll(async (done) => {
 
 afterAll(async (done) => {
   await Hosts.findByIdAndUpdate('60020bda53702f682cd8968c', { messages: [] });
+  // request.close();
+  // db.close();
   done();
 });
 
