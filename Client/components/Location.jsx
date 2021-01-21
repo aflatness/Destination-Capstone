6e86b0cade58f3e2567faf79b3f1246cbebbc7be
aflatness <aflatness@gmail.com>
2021-01-21 15:45:50 -0700

    Map overlay showing as a modal. Should cover entire screen. Fixed amp circle issue!!!

--- Client/components/Location.jsx
+++ Client/components/Location.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Modal } from 'react-bootstrap';
 import initMap from '../gMap.js';
 
 const Location = ({ location }) => {
   const [isShown, setModule] = useState(false);
-  const maps = useRef('map');
 
   const {
     city, state, country, desc,
@@ -12,54 +12,15 @@ const Location = ({ location }) => {
   const query = `${city}, ${state}, ${country}`;
 
   useEffect(() => {
-    (async () => {
-      await initMap(query);
-    })();
+    if (city) {
+      console.log(query);
+      initMap(query);
+    }
   }, [isShown, location]);
 
-  if (isShown) {
-    const pars = desc.split('\n\n').map((par) => par.split(':'));
-    return (
-      <div id='loc-overlay'>
-        <button type='button' onClick={() => setModule(false)}>{'<'}</button>
-        <div id='loc-info'>
-          <div>
-            <h1>Location</h1>
-            <div className='loc-title '>
-              <h6>{`${city}, ${state}, ${country}`}</h6>
-            </div>
-            <div>
-              <div id='loc-desc'>
-                {pars.map((par) => (
-                  par.length > 1
-                    ? (
-                      <div key={par}>
-                        <h4>{par[0]}</h4>
-                        <p>{par[1]}</p>
-                      </div>
-                    )
-                    : (
-                      <div key={par}>
-                        <p>{par}</p>
-                      </div>
-                    )
-                ))}
-              </div>
-            </div>
-          </div>
-          <div>
-            <div id='map' ref={maps} />
-            {/* <div id='loc-notice'>Exact location provided after booking</div>
-            <div id='sel-transit'>
-              <label htmlFor='transit'>
-                <input id='transit' type='checkbox' value='Transit' />
-                Public Transit
-              </label>
-            </div> */}
-          </div>
-        </div>
-      </div>
-    );
+  let pars = [];
+  if (desc) {
+    pars = desc.split('\n\n').map((par) => par.split(':'));
   }
 
   return (
@@ -67,12 +28,12 @@ const Location = ({ location }) => {
       <hr />
       <h4 className='loc-title'>Location</h4>
       <div id='wrapper'>
-        <div id='map' ref={maps} />
-        <div id='loc-notice'>Exact location provided after booking</div>
-        <div id='sel-transit'>
+        {!isShown && <div id='map' />}
+        <div className='loc-notice'>Exact location provided after booking</div>
+        <div className='sel-transit'>
           <label htmlFor='transit'>
             <input id='transit' type='checkbox' value='Transit' />
-            Public Transit
+            <div id='public-transit'>Public Transit</div>
           </label>
         </div>
       </div>
@@ -90,6 +51,56 @@ const Location = ({ location }) => {
       <br />
       <button type='button' onClick={() => setModule(true)}>More about the location</button>
       <hr />
+      <Modal
+        show={isShown}
+        onHide={() => setModule(false)}
+        centered
+        animation
+        dialogClassName='loc-modal'
+      >
+        <Modal.Body>
+          <div id='loc-overlay'>
+            <button type='button' onClick={() => setModule(false)}>{'<'}</button>
+            <div id='loc-info'>
+              <div>
+                <h1>Location</h1>
+                <div className='loc-title '>
+                  <h6>{`${city}, ${state}, ${country}`}</h6>
+                </div>
+                <div>
+                  <div id='loc-desc'>
+                    {pars.map((par) => (
+                      par.length > 1
+                        ? (
+                          <div key={par}>
+                            <h4>{par[0]}</h4>
+                            <p>{par[1]}</p>
+                          </div>
+                        )
+                        : (
+                          <div key={par}>
+                            <p>{par}</p>
+                          </div>
+                        )
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div id='map'>
+                  <div className='loc-notice loc-notice-modal'>Exact location provided after booking</div>
+                  <div className='sel-transit sel-transit-modal'>
+                    <label htmlFor='transit'>
+                      <input id='transit' type='checkbox' value='Transit' />
+                      <div id='public-transit'>Public Transit</div>
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
